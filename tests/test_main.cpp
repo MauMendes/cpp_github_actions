441ae48626c7ea3e8dac3ff32cf69ef8e3315987
Mauricio Mendes <mauricio.eletrica@gmail.com>
2024-04-25 21:06:29 +0200

    fix for running gtest

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -6,3 +6,13 @@
 TEST(ExampleTest, Example) {
     ASSERT_EQ(1, 1);
 }
+
+// Define another test case
+TEST(ExampleTest, Test2) {
+    ASSERT_TRUE(true);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
